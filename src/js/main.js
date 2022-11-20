cd8cca8b0a6cae69fce6bff1e7f4b5135c07702e
islom-karimov <islom.kdev@gmail.com>
2022-11-20 19:07:40 +0500

    feat: swiper bundle

--- src/js/main.js
+++ src/js/main.js
@@ -1,19 +1,20 @@
-// my scripts
-console.log("Welcome");
-
-// Library
+// Libraries
 import Bootstrap from "bootstrap";
-import fancybox from "@fancyapps/ui";
-import jQuery from "jquery";
-import Swiper from "swiper";
+// import fancybox from "@fancyapps/ui";
+// import jQuery from "jquery";
+import Swiper from "swiper/bundle";
+import { gsap } from "gsap"
 
 // Only Swiper scripts
 const myy = new Swiper(".mySwiper", {
-    slidesPerView: 3,
-    spaceBetween: 30,
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
+  slidesPerView: 3,
+  spaceBetween: 30,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
 });
-
